6a45dcd27846d76b6e6ca185c6d220b2294381fa
dgerth5 <davidgerth5@gmail.com>
2024-12-22 17:34:00 -0600

    messed up api call but this should be better to pull in all lines from 1 hr to post

--- get_game_info.R
+++ get_game_info.R
@@ -1,12 +1,16 @@
 library(hockeyR)
 library(lubridate)
+library(tidyverse)
+library(httr)
+library(jsonlite)
+library(purrr)
 
 games21 <- get_game_ids(2021)
 games22 <- get_game_ids(2022)
 games23 <- get_game_ids(2023)
 games24 <- get_game_ids(2024)
 
-master_games <- bind_rows(games21, games22, games23, games24)
+master_games <- bind_rows(games21, games22, games23)
 master_games$iso_game_time <- format(as.POSIXct(paste0(master_games$date, master_games$game_time),
                                                 format = "%Y-%m-%d %I:%M %p", 
                                                 tz = "UTC"), "%Y-%m-%dT%H:%M:%SZ") # time format required for API
@@ -15,9 +19,53 @@ master_games$iso_game_time_hour_before <- format(as.POSIXct(paste0(master_games$
                                                             tz = "UTC") - hours(1), "%Y-%m-%dT%H:%M:%SZ") # for getting lines 1 hour before game time
 
 unique_iso_gt <- unique(master_games$iso_game_time_hour_before)
-length(unique(master_games$date))
 
 
 url_formula <- function(key, datetime){
   url <- paste0("https://api.the-odds-api.com/v4/historical/sports/icehockey_nhl/odds?regions=us&markets=totals&oddsFormat=american&apiKey=",key,"&date=", datetime)
 }
+
+urls <- url_formula(key, unique_iso_gt)
+
+get_data_from_url <- function(url) {
+  response <- GET(url)
+  data_parsed <- content(response, as = "parsed")
+  print(response[["headers"]][["x-requests-remaining"]])
+  
+  if (is.null(data_parsed$data)) return(NULL)
+  
+  main_timestamp <- data_parsed$timestamp
+  games <- data_parsed$data
+  
+  map_dfr(games, function(game) {
+    # iterating across book makers
+    map_dfr(game$bookmakers, function(bookmaker) {
+      
+      # extract only totals
+      totals_market <- keep(bookmaker$markets, ~ .x$key == "totals")
+      if (length(totals_market) == 0) return(NULL)
+      
+      # get outcomes
+      outcomes <- totals_market[[1]]$outcomes
+      
+      # over/Under
+      over_info <- keep(outcomes, ~ .x$name == "Over")[[1]]
+      under_info <- keep(outcomes, ~ .x$name == "Under")[[1]]
+      
+      data.frame(
+        timestamp = main_timestamp,
+        home_team = game$home_team,
+        away_team = game$away_team,
+        sportsbook = bookmaker$key, 
+        over_price = over_info$price,
+        over_point = over_info$point,
+        under_price = under_info$price,
+        under_point = under_info$point
+      )
+    })
+  })
+  Sys.sleep(1)
+}
+
+hockey_totals_22_23 <- map_dfr(urls, get_data_from_url)
+

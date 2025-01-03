bd5181c9a4a1bb11089754e8d529ad73a545856c
dgerth5 <davidgerth5@gmail.com>
2025-01-02 23:08:04 -0600

    trying dk

--- in_play_totals_model.R
+++ in_play_totals_model.R
@@ -56,10 +56,12 @@ sd(test_df$future_goals) # rmse below sd so there is some signal
 #### test how odds help ####
 
 clean_game_lines <- game_lines %>%
-  mutate(id = paste0(lubridate::date(timestamp),"-",home_team,"-",away_team)) %>%
-  group_by(id) %>%
-  summarise(median_line = median(over_point)) %>%
-  ungroup()
+  mutate(id = paste0(lubridate::date(timestamp),"-",home_team,"-",away_team),
+         over_ip = if_else(over_price < 0, over_price / (over_price - 100), 100 / (over_price + 100)),
+         under_ip = if_else(under_price < 0, under_price / (under_price - 100), 100 / (under_price + 100)),
+         over_devig = over_ip / (over_ip + under_ip))%>%
+  filter(sportsbook == "draftkings") %>%
+  distinct(id, .keep_all = TRUE)
 
 clean_df_function2 <- function(data){
   
@@ -77,7 +79,7 @@ clean_df_function2 <- function(data){
     dplyr::select(id, future_goals, is_ev, cur_goals, goal_dif, game_seconds_remaining, rolling_shots) %>%
     # dplyr::select(future_goals, is_ev, key_faceoff, cur_goals, game_seconds_remaining) %>%
     #   filter(game_seconds_remaining <1200) %>% # trying just 3rd period
-    #    filter(game_seconds_remaining <2400) %>%
+    #    filter(game_seconds_remaining 2400) %>%
     #   filter(game_seconds_remaining >0) %>% # remove OT
     drop_na()
   
@@ -89,8 +91,19 @@ clean_df_function2 <- function(data){
 
 master2 <- bind_rows(clean_df_function2(pbp22),
                      clean_df_function2(pbp23)) %>%
-  left_join(clean_game_lines, by = "id")
+  left_join(clean_game_lines, by = "id") %>%
+  drop_na()
 
-mod4 <- lm(future_goals ~ rolling_shots + is_ev + cur_goals + goal_dif + game_seconds_remaining * median_line, 
+mod3 <- lm(future_goals ~ rolling_shots + is_ev + cur_goals + goal_dif + game_seconds_remaining , 
            data = master2)
+mod4 <- mgcv::bam(future_goals ~  s(rolling_shots) + is_ev + s(cur_goals) + s(goal_dif) + game_seconds_remaining + s(over_point, over_ip), 
+           data = master2)
+
+pred <- predict(mod3, master2)
+pred2 <- predict(mod4, master2)
+
+rmse(master2$future_goals, pred2)
+
+anova(mod3,mod4)
+
 summary(mod4)

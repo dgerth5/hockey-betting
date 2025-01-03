3a95f252366185d8a81883a68a29562c8810338c
dgerth5 <davidgerth5@gmail.com>
2025-01-02 23:07:33 -0600

    switched to dk, working better

--- totals_efficiency.R
+++ totals_efficiency.R
@@ -13,47 +13,30 @@ master_games <- bind_rows(games22, games23) %>%
 # read betting data
 totals_data <- read_csv("hockey_lines_22-23.csv")
 
-# just lowvig for now
 # using additive method for devigging
-# totals_join_df <- totals_data %>%
-#   filter(sportsbook == "lowvig") %>%
-#   mutate(over_ip = if_else(over_price < 0, over_price / (over_price - 100), 100 / (over_price + 100)),
-#          under_ip = if_else(under_price < 0, under_price / (under_price - 100), 100 / (under_price + 100)),
-#          over_devig = over_ip / (over_ip + under_ip),
-#          id = paste0(lubridate::date(timestamp),"-",home_team,"-",away_team)) %>%
-#   distinct(id, .keep_all = TRUE)
-
 totals_join_df <- totals_data %>%
-  mutate(id = paste0(lubridate::date(timestamp),"-",home_team,"-",away_team)) %>%
-  group_by(id) %>%
-  summarise(mean_goals = mean(over_point, na.rm = TRUE)) %>%
-  ungroup()
+  filter(sportsbook == "lowvig") %>%
+  mutate(over_ip = if_else(over_price < 0, over_price / (over_price - 100), 100 / (over_price + 100)),
+         under_ip = if_else(under_price < 0, under_price / (under_price - 100), 100 / (under_price + 100)),
+         over_devig = over_ip / (over_ip + under_ip),
+         id = paste0(lubridate::date(timestamp),"-",home_team,"-",away_team)) %>%
+  distinct(id, .keep_all = TRUE) # not sure why theres 4581 games not 2801
 
 
-
-# # complete dataframe
-# join_df <- master_games %>%
-#   select(date, home_name, away_name, id, total) %>%
-#   left_join(totals_join_df %>% select(id, mean_goals), by = "id") %>% 
-#   drop_na() %>%
-#   mutate(p_over = if_else(over_devig > 0.5, 1, 0),
-#          tot_over = if_else(total > over_point, 1, 0))
-
-
+# complete dataframe
 join_df <- master_games %>%
   select(date, home_name, away_name, id, total) %>%
-  left_join(totals_join_df %>% select(id, mean_goals), by = "id")  %>%
-  drop_na()
+  left_join(totals_join_df %>% select(id, over_ip, over_point), by = "id") %>%
+  drop_na() %>%
+  group_by(over_point) %>%
+  summarise(mean_ip = mean(over_ip),
+            mean_goals = mean(total),
+            sd_goals = sd(total),
+            n = n()) %>%
+  filter(n > 10)
 
+t5.5 <- join_df2 %>%
+  filter(over_point == 7) 
 
-mod1 <- lm(total ~ mean_goals, data = join_df)
-summary(mod1)
-
-mod2 <- glm(tot_over ~ p_over, join_df, family = "binomial")
-summary(mod2)
-pred_mod2 <- predict(mod2, join_df, type = "response")
-
-library(pROC)
-roc <- roc(join_df$tot_over, pred_mod2)
-auc(roc)
-pROC::plot.roc(roc)
+hist(t5.5$total, freq = FALSE, breaks = 10)
+lines(dpois(1:12, 7), col = "red")
